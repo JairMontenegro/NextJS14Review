0026caedcfee54e00d9cd97465a1c36561115d6c
jair <jairmontenegro@flare.com.co>
2024-06-06 14:33:16 -0500

    feat: layout added

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
 export default function Page() {
-  // flle system routing  basically. The project needs a a systems of folders that allow us to routing our pages project, it means that we need create a folder with any name that you want and inside it you have to create a page.tsx
-  return <h1>holi</h1>;
+  // flle system routing  basically. the project needs a a systems of folders that allow us to routing our pages project, it means that we need create a folder with any name that you want and inside it you have to create a page.tsx
+  return <h1>este es el contenido del dashboard</h1>;
 }
