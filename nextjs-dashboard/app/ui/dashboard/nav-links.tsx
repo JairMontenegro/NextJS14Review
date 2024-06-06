89982611e3f4992dc664484eb879000c6c050bbf
jair <jairmontenegro@flare.com.co>
2024-06-06 15:34:05 -0500

    feat: adding link component and use client explanation

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,10 +1,13 @@
+'use client'; // is importat becouse all compnents that we are using, they are in de sever, so when i want to use some javascript logic i need use "use client"
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -19,16 +22,19 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathName = usePathname();
   return (
     <>
       {links.map((link) => {
         // link
         const LinkIcon = link.icon;
         return (
-          <Link // The Link component allows us to set up a next navigation that does not show the entire page, but rather the part we need
+          <Link // The Link component allows us to set up a Next.js navigation that does not reload the entire page, but rather only the part we need. Additionally, the Link component performs automatic code splitting, which makes your app divide into smaller chunks or bundles automatically. Next.js preloads any Link component that is visible in your viewport, so when the user clicks, the load will be faster.
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3 ${
+              pathName === link.href ? 'bg-sky-100 text-blue-600' : ''
+            }`}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
