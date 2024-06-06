3e3aefd818099b7126343d6fd50ec226c6bfa998
jair <jairmontenegro@flare.com.co>
2024-06-06 14:49:48 -0500

    feat: link next component

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -4,6 +4,8 @@ import {
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 
+import Link from 'next/link';
+
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
@@ -20,16 +22,17 @@ export default function NavLinks() {
   return (
     <>
       {links.map((link) => {
+        // link
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link // The Link component allows us to set up a next navigation that does not show the entire page, but rather the part we need
             key={link.name}
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
