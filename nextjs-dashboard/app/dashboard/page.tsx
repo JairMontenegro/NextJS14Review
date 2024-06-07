845cdef5eceaf6d02556987f18f067d05703ff6f
jair <jairmontenegro@flare.com.co>
2024-06-07 09:03:43 -0500

    feat: excercise chapter 7

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -1,4 +1,47 @@
-export default function Page() {
-  // flle system routing  basically. the project needs a a systems of folders that allow us to routing our pages project, it means that we need create a folder with any name that you want and inside it you have to create a page.tsx
-  return <h1>este es el contenido del dashboard</h1>;
+// flle system routing  basically. the project needs a a systems of folders that allow us to routing our pages project, it means that we need create a folder with any name that you want and inside it you have to create a page.tsx
+
+// In next JS data fetch is server side, so we don't need to make the fetch in client side becouse the librarys inside makes that fetch too, and lost time. As known as react server component and a particulary thing about react server component is that components can be async
+
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { revenue } from '../lib/placeholder-data';
+import {
+  fetchLatestInvoices,
+  fetchRevenue,
+  fetchCardData,
+} from '@/app/lib/data';
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
+
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
+      </div>
+    </main>
+  );
 }
